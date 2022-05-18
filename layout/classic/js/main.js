af339f80d9ddad55e77e3a7544c5303d9ac6d48d
Majeed Alberawi <majeedmousa0@gmail.com>
2022-05-18 23:27:17 +0300

    Uploaded Files

---
+++ layout/classic/js/main.js
@@ -0,0 +1,22 @@
+const bar = document.querySelector(".bar");
+
+bar.addEventListener("click", () => {
+  bar.classList.toggle("active");
+});
+
+// Filltering //
+const selected = document.querySelector(".selected");
+const optionsContainer = document.querySelector(".options-container");
+
+const optionsList = document.querySelectorAll(".option");
+
+selected.addEventListener("click", () => {
+  optionsContainer.classList.toggle("active");
+});
+
+optionsList.forEach((e) => {
+  e.addEventListener("click", () => {
+    selected.innerHTML = e.querySelector("label").innerHTML;
+    optionsContainer.classList.remove("active");
+  });
+});
